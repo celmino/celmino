8b6a38b8e736c66fc16e247ea7f6a8d0c02040b1
selimtan <stanoncloud@gmail.com>
2024-01-20 23:47:25 +0300

    .

--- app/widget/workspace-tree-widget/src/AppController.ts
+++ app/widget/workspace-tree-widget/src/AppController.ts
@@ -1,4 +1,4 @@
-import { Button, FormBuilder, Fragment, HStack, Heading, Icon, Icons, Loader, LoaderSizes, OptionsContext, Spacer, SvgIcon, Text, UIController, UIView, UIWidget, VStack, cTopLeading, useEffect, useState } from '@tuval/forms';
+import { Button, FormBuilder, Fragment, HStack, Heading, Icon, Icons, Loader, LoaderSizes, OptionsContext, Spacer, Spinner, SvgIcon, Text, UIController, UIView, UIViewBuilder, UIWidget, VStack, cLeading, cTopLeading, cVertical, useEffect, useState } from '@tuval/forms';
 
 import { LeftSideMenuView } from './views/WorkspaceTree';
 import { useGetWorkspaces } from '@celmino/workprotocol';
@@ -7,8 +7,9 @@ import { WorkbenchIcons } from './views/WorkbenchIcons';
 import { AddSpaceDialog, SaveSpaceAction } from './dialogs/AddSpaceDialog';
 import { DynoDialog } from '@realmocean/ui';
 import { getAppletName, getListId } from './utils';
-import { useGetDocument } from '@realmocean/sdk';
+import { useGetDocument, useUpdateDocument } from '@realmocean/sdk';
 import { useLocalStorageState } from './views/localStorageState';
+import { TextField } from '@realmocean/vibe';
 
 
 
@@ -16,6 +17,7 @@ export class MyTestController extends UIController {
 
     public override LoadView(): UIView {
         const [isOpen, setIsOpen] = useState(getAppletName() === 'com.celmino.applet.workmanagement');
+        const [isEditing, setIsEditing] = useState(false);
         const isLoading = false;
         const { items } = this.props.data || {};
         const { selectedItem, team_id, workspaceId, folder_id, appletId, showAllWorkspaces, opas, folder_menu, app_id } = this.props.config || {};
@@ -39,72 +41,98 @@ export class MyTestController extends UIController {
         }, []);
 
         const [expanded, setExpanded] = useLocalStorageState('work_management_tree', false);
+        const { document: applet, isLoading: isAppletLoading } = useGetDocument({ projectId: workspaceId, databaseId: 'workspace', collectionId: 'applets', documentId: appletId })
 
         return (
-            OptionsContext(() => (
-                VStack({ alignment: cTopLeading })(
+            isAppletLoading ? Spinner() :
+                OptionsContext(() => (
+                    VStack({ alignment: cTopLeading })(
 
-                    HStack({ alignment: cTopLeading, spacing: 1 })(
-                        HStack(
+                        HStack({ alignment: cLeading, spacing: 1 })(
                             HStack(
-                                isLoading ? Loader().size(LoaderSizes.XS) :
-                                    Icon(WorkbenchIcons.CaretDown).transform(isOpen ? '' : 'rotate(-90deg)')
-                            ).width(20).height(20).cursor('pointer')
-                                .display(`var(--display-caret)`),
-                            /* HStack(
-                                UIWidget("com.tuvalsoft.widget.icons")
-                                    .config({
-                                        selectedIcon: 'bookmark',
-                                        color: 'gray',
-                                        width: 18,
-                                        height: 18,
-                                        padding: 1
-                                    })
-                            )
-                                .width().height()
-                                .cornerRadius(5)
-                                .display('var(--display-icon)'), */
-                        ).width(20).height(20)
-                            .onClick(() => {
-                                setIsOpen(!isOpen);
-                            }),
-                        /*   HStack(
-                              isLoading ? Loader().size(LoaderSizes.XS) :
-                                  Icon(SvgIcon('cu3-icon-caret', 'gray', '1em','1em')).transform(isOpen ?  'rotate(90deg)': '')
-                          ).width().height()
-                              .onClick(() => {
-                                  setIsOpen(!isOpen);
-                              }), */
-                        Heading('Work Management').fontSize(16)
-                            //  .fontFamily('Figtree, Roboto, "Noto Sans Hebrew", "Noto Kufi Arabic", "Noto Sans JP", sans-serif')
-                            //  .fontWeight('600')
-                            .lineHeight(22),
+                                HStack(
+                                    isLoading ? Loader().size(LoaderSizes.XS) :
+                                        Icon(WorkbenchIcons.CaretDown).transform(isOpen ? '' : 'rotate(-90deg)')
+                                ).width(20).height(20).cursor('pointer')
+                                    .display(`var(--display-caret)`),
 
-                        Spacer(),
-                        HStack(
-                            Icon(Icons.Add).size(15)
-                        ).height(20).width(20)
-                            .background('gray')
-                            .onClick(() => {
+                            ).width(20).height(20)
+                                .onClick(() => {
+                                    setIsOpen(!isOpen);
+                                }),
 
-                                DynoDialog.Show(AddSpaceDialog(workspaceId,appletId, '/'));
+                            // Title
+                            isEditing ? UIViewBuilder(() => {
+
+                                const [appletName, setAppletName] = useState(applet.name);
+                                const { updateDocument } = useUpdateDocument(workspaceId);
+                                return (
+                                    HStack(
+                                        TextField()
+                                        .fontSize(16)
+                                        .fontWeight('500')
+                                        .value(appletName)
+                                            .onChange((value) => setAppletName(value))
+                                            .onBlur(() => {
+                                                if (applet.name !== appletName) {
+                                                    updateDocument({
+                                                        databaseId: 'workspace',
+                                                        collectionId: 'applets',
+                                                        documentId: appletId,
+                                                        data: {
+                                                            name: appletName
+                                                        }
+                                                    });
+                                                }
+                                                setIsEditing(false);
+                                            })
+                                    )
+                                        .height()
+                                        .onClickAway(() => {
+                                            if (applet.name !== appletName) {
+                                                updateDocument({
+                                                    databaseId: 'workspace',
+                                                    collectionId: 'applets',
+                                                    documentId: appletId,
+                                                    data: {
+                                                        name: appletName
+                                                    }
+                                                });
+                                            }
+                                            setIsEditing(false);
+                                        })
+
+                                )
                             })
-                    )
-                        .fontWeight('500')
-                        .height().padding(5)
-                        .variable('--show-space-action-buttons', { default: 'none', hover: 'flex' })
-                    // .variable(`--display-caret`, { default: 'none', hover: 'flex' })
-                    // .variable(`--display-icon`, { default: 'flex', hover: 'none' })
-                    ,
-                    isOpen ?
-                        HStack(
-                            LeftSideMenuView(selectedItem)
-                        ).height().paddingLeft('30px') : Fragment()
-                ).height()
-            ))
-                .options({
-                    ...(this.props.config || {})
-                })
+                                :
+                                Heading(applet.name).fontSize(16)
+
+                                    .lineHeight(22),
+
+                            Spacer(),
+                            HStack(
+                                Icon(Icons.Add).size(15)
+                            ).height(20).width(20)
+                                .background('gray')
+                                .onClick(() => {
+                                    setIsEditing(!isEditing)
+                                    // DynoDialog.Show(AddSpaceDialog(workspaceId, appletId, '/'));
+                                })
+                        )
+                            .fontWeight('500')
+                            .allHeight(37).padding(5).padding(cVertical, isEditing ? 0 : 5)
+                            .variable('--show-space-action-buttons', { default: 'none', hover: 'flex' })
+
+                        ,
+                        isOpen ?
+                            HStack(
+                                LeftSideMenuView(selectedItem)
+                            ).height().paddingLeft('30px') : Fragment()
+                    ).height()
+                ))
+                    .options({
+                        ...(this.props.config || {})
+                    })
         )
     }
 }
